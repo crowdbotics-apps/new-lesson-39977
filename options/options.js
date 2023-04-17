90e9034a6ce72412c55fe8c63199c40a981f1fc7
shahraiz-cb <shahraiz.ali@crowdbotics.com>
2023-04-18 02:35:37 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -2,7 +2,7 @@ export const globalOptions = {
   "name": "new-lesson-39977",
   "url": "https://new-lesson-39977.botics.co",
   "api": "https://new-lesson-39977.botics.co/api/v1",
-  "initialRoute": "many-rows"
+  "initialRoute": "Untitled1"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,15 +1,23 @@
+import { Text } from "react-native";
 import React from "react";
 import { StyleSheet, SafeAreaView } from "react-native";
 
 const Untitled1 = () => {
   return <SafeAreaView style={styles.safeArea}>
         
-    </SafeAreaView>;
+    <Text style={styles.kpwVUzJV}>Lorem ipsum…</Text></SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     height: '100%'
+  },
+  kpwVUzJV: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
   }
 });
 export default Untitled1;
