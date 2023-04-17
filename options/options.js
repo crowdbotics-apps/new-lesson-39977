1c8032af91b7d2631bef69649a943ef2da774597
Guilherme Livramento <guilherme.livramento.fsl@crowdbotics.com>
2023-04-17 09:15:59 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "new-lesson-39977",
-  url: "https://new-lesson-39977.botics.co",
-  api: "https://new-lesson-39977.botics.co/api/v1"
-}
+  "name": "new-lesson-39977",
+  "url": "https://new-lesson-39977.botics.co",
+  "api": "https://new-lesson-39977.botics.co/api/v1",
+  "initialRoute": "manyRows"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
---
+++ screens/Untitled1/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet, SafeAreaView } from "react-native";
+
+const Untitled1 = () => {
+  return <SafeAreaView style={styles.safeArea}>
+        
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: '100%'
+  }
+});
+export default Untitled1;
