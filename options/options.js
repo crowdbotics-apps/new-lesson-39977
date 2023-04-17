81787a8fa53d920cb3ef436315ac5392540491ad
Guilherme Livramento <guilherme.livramento.fsl@crowdbotics.com>
2023-04-17 09:54:27 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -2,7 +2,7 @@ export const globalOptions = {
   "name": "new-lesson-39977",
   "url": "https://new-lesson-39977.botics.co",
   "api": "https://new-lesson-39977.botics.co/api/v1",
-  "initialRoute": "manyRows"
+  "initialRoute": "many-rows"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
--- screens/many-rows/index.js
+++ screens/many-rows/index.js
@@ -1,10 +1,15 @@
+import { useNavigation } from "@react-navigation/native";
+import { Pressable } from "react-native";
 import React, { useEffect } from "react";
 import { FlatList, Text, View, StyleSheet } from "react-native";
 
 const Row = ({
   item
 }) => {
-  return <View style={styles.row}>
+  const navigation = useNavigation();
+  return <Pressable onPress={() => {
+    navigation.navigate("eventDetails");
+  }}><View style={styles.row}>
         <View style={styles.rowContent}>
           <Text style={styles.textHeading1}>{item.name}</Text>
           <Text style={styles.textHeading2}>Heading 2</Text>
@@ -12,7 +17,7 @@ const Row = ({
           <Text style={styles.textHeading4}>Heading 4</Text>
           <Text style={styles.textDescription}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s</Text>
         </View>
-      </View>;
+      </View></Pressable>;
 };
 
 const ManyRows = () => {
