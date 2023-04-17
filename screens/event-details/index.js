be103b840d4f20e2243ea8cf3f69475f3d8a4fc6
shahraiz-cb <shahraiz.ali@crowdbotics.com>
2023-04-18 03:22:51 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/event-details/index.js
+++ screens/event-details/index.js
@@ -35,7 +35,7 @@ const EventDetails = ({
         <View style={styles.body}>
           <View style={styles.header}>
             <View style={styles.details}>
-              <Text style={styles.name}>{Recipes?.title}</Text>
+              <Text style={styles.name}>{Recipes?.[0]?.title}</Text>
               <View style={styles.location}>
                 <Image source={require("./assets/locationIcon.png")} style={styles.locationIcon} />
                 <Text style={styles.locationText}>
