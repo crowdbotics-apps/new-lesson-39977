d4d213cc2b3aa463d40f753e447c50a80b4eb006
Guilherme Livramento <guilherme.livramento.fsl@crowdbotics.com>
2023-04-18 11:22:05 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled4/index.js
+++ screens/Untitled4/index.js
@@ -1,16 +1,32 @@
+import { api_v1_recipe_create } from "../../store/newlessonAPI/recipes.slice.js";
+import { useDispatch } from "react-redux";
+import { Pressable } from "react-native";
+import { View } from "react-native";
 import { TextInput } from "react-native";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
 const Untitled4 = () => {
+  const dispatch = useDispatch();
   return <SafeAreaView style={styles.safeArea}>
         <ScrollView contentContainerStyle={{
       backgroundColor: '#f0f0f1',
       padding: 10,
       position: 'relative',
       flex: 1
-    }}><TextInput style={styles.DlpCZwyY}></TextInput><TextInput style={styles.qnDBOseA}></TextInput><TextInput style={styles.YoeRUnmA}></TextInput><TextInput style={styles.szJrrPKB}></TextInput><TextInput style={styles.afmtOhRw}></TextInput><TextInput style={styles.XsVmWJJX}></TextInput></ScrollView>
+    }}><TextInput style={styles.DlpCZwyY}></TextInput><TextInput style={styles.qnDBOseA}></TextInput><TextInput style={styles.YoeRUnmA}></TextInput><TextInput style={styles.szJrrPKB}></TextInput><TextInput style={styles.afmtOhRw}></TextInput><TextInput style={styles.XsVmWJJX}></TextInput><Pressable onPress={onSubmit}><View style={styles.adVcCPOj}></View></Pressable></ScrollView>
     </SafeAreaView>;
+
+  const onSubmit = () => {
+    dispatch(api_v1_recipe_create({
+      title,
+      instructions,
+      cook_time,
+      prep_time,
+      rating,
+      image
+    }));
+  };
 };
 
 const styles = StyleSheet.create({
@@ -27,7 +43,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#ffffff",
     borderColor: "#cccccc",
     width: 150,
-    height: 30
+    height: 30,
+    top: 0
   },
   YoeRUnmA: {
     backgroundColor: "#ffffff",
@@ -52,6 +69,13 @@ const styles = StyleSheet.create({
     borderColor: "#cccccc",
     width: 150,
     height: 30
+  },
+  adVcCPOj: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
   }
 });
 export default Untitled4;
