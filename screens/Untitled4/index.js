90d6a3fe7c05cc6051eac68912f4c275c2023781
Guilherme Livramento <guilherme.livramento.fsl@crowdbotics.com>
2023-04-18 11:19:27 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled4/index.js
+++ screens/Untitled4/index.js
@@ -1,3 +1,4 @@
+import { TextInput } from "react-native";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
@@ -8,13 +9,49 @@ const Untitled4 = () => {
       padding: 10,
       position: 'relative',
       flex: 1
-    }}></ScrollView>
+    }}><TextInput style={styles.DlpCZwyY}></TextInput><TextInput style={styles.qnDBOseA}></TextInput><TextInput style={styles.YoeRUnmA}></TextInput><TextInput style={styles.szJrrPKB}></TextInput><TextInput style={styles.afmtOhRw}></TextInput><TextInput style={styles.XsVmWJJX}></TextInput></ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     height: '100%'
+  },
+  DlpCZwyY: {
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 150,
+    height: 30
+  },
+  qnDBOseA: {
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 150,
+    height: 30
+  },
+  YoeRUnmA: {
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 150,
+    height: 30
+  },
+  szJrrPKB: {
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 150,
+    height: 30
+  },
+  afmtOhRw: {
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 150,
+    height: 30
+  },
+  XsVmWJJX: {
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 150,
+    height: 30
   }
 });
 export default Untitled4;
